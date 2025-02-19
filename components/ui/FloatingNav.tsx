22c23c12177a0e4d430c0e3190a9516876986a3a
Janki23193 <jankirawal93@gmail.com>
2025-02-19 07:26:51 +0530

    fixed top menu

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -8,6 +8,7 @@ import {
 } from "framer-motion";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 export const FloatingNav = ({
   navItems,
@@ -23,7 +24,10 @@ export const FloatingNav = ({
   const { scrollYProgress } = useScroll();
 
   const [visible, setVisible] = useState(false);
-
+   const [menuOpen, setMenuOpen] = useState(false); 
+  const toggle = () =>{
+    setMenuOpen(!menuOpen);
+  }
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     // Check if current is not undefined and is a number
     if (typeof current === "number") {
@@ -32,16 +36,22 @@ export const FloatingNav = ({
       if (scrollYProgress.get() < 0.05) {
         setVisible(false);
       } else {
-        if (direction < 0) {
-          setVisible(true);
-        } else {
-          setVisible(false);
-        }
+        setVisible(direction < 0)
+        // if (direction < 0) {
+        //   setVisible(true);
+        // } else {
+        //   setVisible(false);
+        // }
       }
     }
   });
 
   return (
+    <>
+    <button onClick={toggle} className="sm:hidden text-white text-2xl z-[6000] fixed top-5 left-5">
+        {menuOpen ? <FaTimes/> : <FaBars/>}
+    </button>
+    <div className="hidden sm:flex">
     <AnimatePresence mode="wait">
       <motion.div
         initial={{
@@ -72,8 +82,49 @@ export const FloatingNav = ({
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
-        
+       
       </motion.div>
     </AnimatePresence>
+    </div>
+      <AnimatePresence>
+       {
+        menuOpen && (
+          <motion.div
+          initial={{
+            opacity: 1,
+            y: -100,
+          }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.2,
+          }}
+          className={cn(
+            "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border dark:border-white/[0.2] rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100",
+            className
+          )}
+          > 
+           {navItems.map((navItem: any, idx: number) => (
+          <Link
+            key={`link=${idx}`}
+            href={navItem.link}
+            className={cn(
+              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+            )}
+            onClick={()=> setMenuOpen(false)}
+          >
+          
+             {navItem.name}
+          </Link>
+        ))}
+          </motion.div>
+        )
+       }
+      
+    </AnimatePresence>
+    </>
+   
   );
 };
